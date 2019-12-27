0596b252d8a270978622bef62e3808fa0f7b6854
David Sacharny <david.sacharny@georq.com>
2019-12-26 21:53:19 -0700

    updated lane drawing

--- base_classes/multi_lane_classes/Lane.m
+++ base_classes/multi_lane_classes/Lane.m
@@ -3,40 +3,87 @@ classdef Lane < handle
     %   Detailed explanation goes here
     
     properties
-        x0
-        xf
+        m_x0
+        m_xf
         m_x0str
         m_xfstr
         vn
         m_id;
         m_graph;
+        m_roi;
+        m_listeners = []
+    end
+    
+    events
+      NodeChanged
+      NodeChanging
     end
     
     methods
-        function obj = Lane(x0, xf)
+        function obj = Lane(m_x0, m_xf)
             %LANE Construct an instance of this class
             %   Detailed explanation goes here
             
             % Flip them to the correct orientation
-            if size(x0,2) > size(x0,1)
-                x0 = x0';
+            if size(m_x0,2) > size(m_x0,1)
+                m_x0 = m_x0';
             end
             
-            if size(xf,2) > size(xf,1)
-                xf = xf';
+            if size(m_xf,2) > size(m_xf,1)
+                m_xf = m_xf';
             end
             
-            obj.x0 = x0;
-            obj.xf = xf;
-            obj.vn = (xf-x0)/norm(xf-x0);
+            obj.m_x0 = m_x0;
+            obj.m_xf = m_xf;
+            obj.vn = (m_xf-m_x0)/norm(m_xf-m_x0);
         end
         
-        function x0 = getStart(obj)
-           x0 = obj(1).x0; 
+        function s = getSumOfDegrees(lane, node_id)
+            in_d = lane.m_graph.indegree(node_id);
+            out_d = lane.m_graph.outdegree(node_id);
+            s = in_d + out_d;
+        end
+        
+        function id = getUniqueNodeId(~)
+            id = char(java.util.UUID.randomUUID);
+        end
+        
+        function handleRoiMove(lane, src, evt)
+            evname = evt.EventName;
+            %disp(['ROI for lane: ' num2str(lane.m_id)]);
+            %v = evt.CurrentPosition - evt.PreviousPosition; 
+              
+            switch(evname)
+                case{'MovingROI'}
+                    % Move the node
+                    %lane.m_x0 = lane.m_x0 + v(1,:)';
+                    %lane.m_xf = lane.m_x0 + v(2,:)';
+                    notify(lane, 'NodeChanging');
+                case{'ROIMoved'}
+                    v = evt.CurrentPosition - [lane.m_x0'; lane.m_xf'];
+                    lane.m_x0 = lane.m_x0 + v(1,:)';
+                    lane.m_xf = lane.m_xf + v(2,:)';
+                    
+                    if (norm(v(1,:)) > 0) && ...
+                            (lane.getSumOfDegrees(lane.m_x0str) > 1)
+                        lane.m_x0str = lane.getUniqueNodeId();
+                    end
+                    
+                    if (norm(v(2,:)) > 0) && ...
+                            (lane.getSumOfDegrees(lane.m_xfstr) > 1)
+                        lane.m_xfstr = lane.getUniqueNodeId();
+                    end
+                    disp("Notifying nodechanged");
+                    notify(lane, 'NodeChanged');
+            end
+        end
+        
+        function m_x0 = getStart(obj)
+           m_x0 = obj(1).m_x0; 
         end
         
         function inLane = isInLane(obj, x)
-            inLane = norm(x-obj.x0) < norm(obj.xf-obj.x0);
+            inLane = norm(x-obj.m_x0) < norm(obj.m_xf-obj.m_x0);
         end
         
         function lanes = getShortestPath(obj, x0_node, xf_node)
@@ -69,31 +116,35 @@ classdef Lane < handle
             end
         end
         
-        function digr = getDigraph(obj)
+        function digr = getDigraph(lanes)
             s = {};
             t = {};
-            weights = zeros(1,length(obj));
-            for i = 1:length(obj)
-                l = obj(i);
+            weights = zeros(1,length(lanes));
+            for i = 1:length(lanes)
+                l = lanes(i);
                 s{i} = l.m_x0str;
                 t{i} = l.m_xfstr;
-                weights(i) = norm(l.xf-l.x0);
+                weights(i) = norm(l.m_xf-l.m_x0);
             end
             digr = digraph(s,t,weights);
+            for i = 1:length(lanes)
+                l = lanes(i);
+                l.m_graph = digr;
+            end
         end
         
-        function [xi, lane_i] = getNextPosition(obj,x0,speed,T,lane_i)
+        function [xi, lane_i] = getNextPosition(obj,m_x0,speed,T,lane_i)
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
-            xi = x0 + speed*obj(lane_i).vn*T;
+            xi = m_x0 + speed*obj(lane_i).vn*T;
             if ~obj(lane_i).isInLane(xi)
                 if length(obj) > lane_i
-                    %xi1 = obj(lane_i).xf - x0;
-                    xdif = xi - obj(lane_i).xf;
+                    %xi1 = obj(lane_i).m_xf - m_x0;
+                    xdif = xi - obj(lane_i).m_xf;
                     lane_i = lane_i + 1;
-                    xi = obj(lane_i).x0 + norm(xdif)*obj(lane_i).vn;
+                    xi = obj(lane_i).m_x0 + norm(xdif)*obj(lane_i).vn;
                 else
-                    xi = obj(lane_i).xf;
+                    xi = obj(lane_i).m_xf;
                 end
             end
         end
@@ -105,8 +156,8 @@ classdef Lane < handle
                 hold(h, 'on');
             end
             for i = 1:length(obj)
-                x = [obj(i).x0(1) obj(i).xf(1)];
-                y = [obj(i).x0(2) obj(i).xf(2)];
+                x = [obj(i).m_x0(1) obj(i).m_xf(1)];
+                y = [obj(i).m_x0(2) obj(i).m_xf(2)];
                 if nargin < 2
                     plot(x, y, '-ok');
                 else
@@ -127,9 +178,9 @@ classdef Lane < handle
                 hold(h, 'on');
             end
             for i = 1:length(obj)
-                x = obj(i).x0(1);
-                y = obj(i).x0(2);
-                v = obj(i).xf - obj(i).x0;
+                x = obj(i).m_x0(1);
+                y = obj(i).m_x0(2);
+                v = obj(i).m_xf - obj(i).m_x0;
                 vx = v(1);
                 vy = v(2);
                 
@@ -168,26 +219,36 @@ classdef Lane < handle
             highlight(plot_handle, s, t);
         end
         
-        function ph = plotLanesDigraph(obj,h)
+        function highlightNodes(~, plot_handle, node_ids)
+            highlight(plot_handle, node_ids);
+        end
+        
+        function ph = plotLanesDigraph(obj,h,callback)
             G = obj.getDigraph;
             x = zeros(2,G.numnodes);
             for i = 1:G.numnodes
                 nodeName = G.Nodes.Name{i};
-                % Find the coordinate for this node
+                % Find the coordinate for this node for plotting the
+                % correct position
                 for j = 1:length(obj)
                     if (strcmp(obj(j).m_x0str, nodeName))
-                        x(:,i) = obj(j).x0;
+                        x(:,i) = obj(j).m_x0;
                         break;
                     elseif (strcmp(obj(j).m_xfstr, nodeName))
-                        x(:,i) = obj(j).xf;
+                        x(:,i) = obj(j).m_xf;
                         break;
                     end
                 end
             end
             if nargin < 2
-                ph = plot(G,'XData',x(1,:)','YData',x(2,:)','EdgeLabel',G.Edges.Weight);
+                ph = plot(G,'XData',x(1,:)','YData',x(2,:)','EdgeLabel',...
+                    G.Edges.Weight);
+            elseif nargin < 3
+                ph = plot(h,G,'XData',x(1,:)','YData',x(2,:)','EdgeLabel',...
+                    G.Edges.Weight);
             else
-                ph = plot(h,G,'XData',x(1,:)','YData',x(2,:)','EdgeLabel',G.Edges.Weight);
+                ph = plot(h,G,'XData',x(1,:)','YData',x(2,:)','EdgeLabel',...
+                    G.Edges.Weight,'ButtonDownFcn', callback);
             end
         end
     end
