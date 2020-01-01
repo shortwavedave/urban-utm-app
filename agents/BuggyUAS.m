5e470f643746c2191859b3dc3e8ff8b300368c66
David Sacharny <david.sacharny@georq.com>
2020-01-01 08:57:35 -0700

    buggy uas

---
+++ agents/BuggyUAS.m
@@ -0,0 +1,62 @@
+classdef BuggyUAS < NormalUAS
+    %BuggyUAS Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        m_rand = [];
+        m_rand_i = 0;
+        m_contingency_per_sec = 1/15;
+        m_contingency_per_step;
+        m_preallocate_rand = 1000;
+        m_desired_speed = 1;
+    end
+    
+    methods
+        function agent = BuggyUAS(ID)
+            %MULTILANEUAS Construct an instance of this class
+            %   Use this function to initialize any instance properties
+            agent = agent@NormalUAS(ID);
+            agent.m_rand = rand(1, agent.m_preallocate_rand);
+            agent.m_contingency_per_step = ...
+                agent.m_contingency_per_sec * agent.m_sample_per;
+        end
+        
+        
+        function chooseAction(uas, action_handle)
+            % Add code here to choose an action
+            will_be_active = uas.getWillBeActive();
+            
+            if will_be_active
+                land_nodes = uas.getLandNodes();
+                launch = uas.m_lane_path.getStartNode();
+                land = land_nodes(randi(length(land_nodes)));
+                lane_path = uas.getShortestPath(launch, land{:});
+                
+                uas.m_lane_path = lane_path;
+            end
+            
+            if (uas.m_active)
+                uas.m_rand_i = uas.m_rand_i + 1;
+                if (uas.m_rand_i > uas.m_preallocate_rand)
+                    uas.m_rand_i = 1;
+                    uas.m_rand = rand(1,uas.m_preallocate_rand);
+                end
+
+                if (uas.m_rand(uas.m_rand_i) < uas.m_contingency_per_step)
+                    uas.setSpeed(0);
+                else
+                    uas.setSpeed(uas.m_desired_speed);
+                end
+            end
+            
+            uas.chooseAction@NormalUAS(action_handle);
+        end
+        
+        function acceptPercept(agent, percept_handle)
+            agent.acceptPercept@NormalUAS(percept_handle);
+            
+            % Add code here to store percepts
+        end
+    end
+end
+
